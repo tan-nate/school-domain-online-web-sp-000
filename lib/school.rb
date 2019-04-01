c5acdc8924ddd16bb81315175b927c1e4a4370ba
Nate Tan <nktan93@gmail.com>
2019-04-01 19:47:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :school_name
+  
+  ROSTER = {}
+  
+  def initialize(school_name)
+    @school_name = school_name
+  end
+  
+  def roster
+    ROSTER
+  end
+  
+  def add_student(student_name, grade)
+    if ROSTER.keys.include?(grade)
+      ROSTER[grade] << student_name
+    else
+      ROSTER[grade] = []
+      ROSTER[grade] << student_name
+    end
+    #binding.pry
+  end
+end
+
+binding.pry
